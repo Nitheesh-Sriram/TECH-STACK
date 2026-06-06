6261bd72b5cf35fef7134a3de644fcd7fc985de0
Nitheesh14125 <s.nitheesh14125@gmail.com>
2026-06-06 14:43:19 +0530

    Iteration implementation

--- OOP'S - WORK /Advanced Python /implementing_iterator.py
+++ OOP'S - WORK /Advanced Python /implementing_iterator.py
@@ -1,1 +1,18 @@
 #problem statement : implement remote control class that allows you to press "next" button to go to next TV channel 
+class remotecontrol():
+    def __init__(self):
+        self.channel = ["HBO","ESPN","ABC","CN"]
+        self.index = -1
+
+    def _iter_(self):
+        return self 
+    
+    def _next_(self):
+        self.index = self.index+1 
+        if self.index == len(self.channel):
+            raise StopIteration 
+        return self.channel[self.index]
+    
+r = remotecontrol()
+itr = iter(r)
+print(next(itr))
